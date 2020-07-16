c5e90b30e0ce794e2d3d98386fff43781a252e5d
tsiou23 <christosal2@gmail.com>
2020-07-16 22:35:08 +0300

    initial commit

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Dr Nice', hp: 420, damage:14 },
+  { id: 12, name: 'Narco', hp: 350, damage:17  },
+  { id: 13, name: 'Bombasto', hp: 600, damage:15 },
+  { id: 14, name: 'Celeritas', hp: 400, damage:16 },
+  { id: 15, name: 'Magneta', hp: 390, damage:15 },
+  { id: 16, name: 'RubberMan', hp: 350, damage:18 },
+  { id: 17, name: 'Dynama', hp: 430, damage:16 },
+  { id: 18, name: 'Dr IQ', hp: 380, damage:15 },
+  { id: 19, name: 'Magma', hp: 400, damage:16 },
+  { id: 20, name: 'Tornado', hp: 450, damage:16 }
+];
